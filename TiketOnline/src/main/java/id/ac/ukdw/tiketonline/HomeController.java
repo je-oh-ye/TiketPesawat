b03c04ec27af107658f55df812f68bc8bbb40950
jeki <jefrison.banni@ti.ukdw.ac.id>
2019-11-19 02:16:22 +0700

    logout

--- TiketOnline/src/main/java/id/ac/ukdw/tiketonline/HomeController.java
+++ TiketOnline/src/main/java/id/ac/ukdw/tiketonline/HomeController.java
@@ -5,17 +5,23 @@
  */
 package id.ac.ukdw.tiketonline;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -40,6 +46,20 @@ public class HomeController implements Initializable {
     private DatePicker date;
     @FXML
     private ComboBox<String> comboboxClass;
+     @FXML
+    private Button btnLogout1;
+
+    @FXML
+    void btnLogout(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/Login.fxml"));
+        Parent Home = loader.load();
+        Scene scene = new Scene(Home);
+        Stage Primarystage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Primarystage.setResizable(false);
+        Primarystage.setScene(scene);
+        Primarystage.show();
+    }
 
     /**
      * Initializes the controller class.
